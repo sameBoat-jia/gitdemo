d50b646975701c6932a82898a4e18d594c6d3fd6
jtz <jtz123123@qq.com>
2022-11-04 16:32:07 +0800

    bbbdfkfljl;dsf

--- src/a/b/c/d/d.java
+++ src/a/b/c/d/d.java
@@ -1,4 +1,10 @@
 package a.b.c.d;
 
 public class d {
+
+
+    public static void main(String[] args) {
+        System.out.println("aaaaaaaaaaaa");
+        System.out.println("bbbbbbbbbbb");
+    }
 }
